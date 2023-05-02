5b5c5d4717f47e28ef4f905545ef55124f79089b
AhmedMagdy25 <am6342071@gmail.com>
2023-05-02 22:15:33 +0300

    update the file

--- RestfulAPI/Controllers/ProductsController.cs
+++ RestfulAPI/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,67 +12,87 @@ namespace RestfulAPI.Controllers
     {
         // GET: api/<ProductsController>
 
+        private string connectionString = "server=127.0.0.1;uid=root;pwd=amP@ssw0rd;database=data";
         [HttpGet]
-        public List<List<string>> GetProducts()
+        public List<string[]> GetProducts()
         {
-            List<List<string>> products = new();
+            List<string[]> products = new();
            
-            string connectionString = "server=127.0.0.1;uid=root;pwd=amP@ssw0rd;database=data";
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
-                connection.Open();
-                MySqlCommand command = new("SELECT id, name, details, rate, price " +
-                    "FROM data.products", connection);
-                MySqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
+                try
                 {
-                    List<string> product = new();
-                    string id = "id: " + reader["id"].ToString();
-                    string name = "name:" + reader["name"].ToString();
-                    string details = "details: " + reader["details"].ToString();
-                    string rate = "rate: " + reader["rate"].ToString();
-                    string price = "price: " + reader["price"].ToString();
+                    connection.Open();
+                    MySqlCommand command = new("SELECT * FROM data.products", connection);
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string[] product;
+                        string id = "id: " + reader["id"].ToString();
+                        string name = "name:" + reader["name"].ToString();
+                        string details = "details: " + reader["details"].ToString();
+                        string rate = "rate: " + reader["rate"].ToString();
+                        string price = "price: " + reader["price"].ToString();
+                        string type = "type: " + reader["type"].ToString();
 
-                    product.Add(id);
-                    product.Add(name);
-                    product.Add(details);
-                    product.Add(rate);
-                    product.Add(price);
+                        product = new string[] { id, name, details, rate, price, type };
 
-                    products.Add(product);
-                }
-                connection.Close();
-                return products;
+                        products.Add(product);
+                    }
+                    connection.Close();
+
+                    return products;
+
+                }catch (Exception e)
+                {
+                    List<string[]> err = new()
+                    {
+                        new string[] { "error: " + e.ToString() }
+                    };
+                    return err;
+                } 
             }
         }
-        [HttpGet("{name}")]
-        public List<string> GetProductByName(string name)
+        [HttpPost("{name},{details},{rate}, {price}, {type}")]
+        public string AddProduct(string name, string details, string rate, double price, string type)
         {
-            List<string> product = new();
-            string connectionString = "server=127.0.0.1;uid=root;pwd=amP@ssw0rd;database=data";
             using (MySqlConnection connection = new(connectionString))
             {
-                connection.Open();
-                MySqlCommand command = new("SELECT id, details, rate, price " +
-                    "FROM data.products WHERE name = \""+name+"\"", connection);
-                MySqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    string id = "id: " + reader["id"].ToString();
-                    string details = "details: " + reader["details"].ToString();
-                    string rate = "rate: " + reader["rate"].ToString();
-                    string price = "price: " + reader["price"].ToString();
+                try {
+                    connection.Open();
+                    MySqlCommand command = new("INSERT INTO data.products "
+                                               + "VALUES(\""+ new Random().Next().ToString() + "\", \"" + name + "\", \"" + details + "\", \""
+                                               + rate + "\", \""+ price + "\", \""+ type +"\");"
+                                               , connection);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    return "the itme is added successfully";
 
-                    product.Add(id);
-                    product.Add(details);
-                    product.Add(rate);
-                    product.Add(price);
-
+                }catch(Exception e)
+                    {
+                    return "error: " + e.ToString();
                 }
-                connection.Close();
-                return product;
+        }
+        }
+        [HttpDelete("{id}")]
+        public string DeleteProductByID(string id)
+        {
+            using (MySqlConnection connection = new(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand command = new("DELETE FROM data.products WHERE id = \""+ id +"\";", connection);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    return "the item is deleted successfully";
+
+                }catch(Exception e)
+                {
+                    return "error: " + e.ToString();
+                }
+                
             }
         }
-
     }
 }
